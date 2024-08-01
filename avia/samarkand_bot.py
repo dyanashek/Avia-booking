cf9391b9a757d7fcca770de58af1ae71dd288b9d
Ianashek Denis <yanashek@gmail.com>
2024-08-01 23:33:13 +0700

    small bot fix

--- avia/samarkand_bot.py
+++ avia/samarkand_bot.py
@@ -29,7 +29,8 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def start_message(message: types.Message):
     user_id = str(message.from_user.id)
-    if await sync_to_async(Manager.objects.filter(telegram_id=user_id).exists)():
+    is_exists = sync_to_async(Manager.objects.filter(telegram_id=user_id).exists)()
+    if is_exists:
         await bot.send_message(chat_id=user_id,
                          text='Привет.\nЗдесь можно контролировать выдачу денежных средств.\nОтправьте код получателя для вывода информации и внесения изменений.',
                          )
