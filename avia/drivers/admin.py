ff5bbf976bbdd03f120e353718113e8cc57605e5
Ianashek Denis <yanashek@gmail.com>
2025-01-16 17:21:00 +0700

    drivers admin

--- avia/drivers/admin.py
+++ avia/drivers/admin.py
@@ -8,3 +8,8 @@ class Driver(admin.ModelAdmin):
     list_display = ('name',)
     fields = ('name', 'telegram_id', 'circuit_id',)
 
+    def get_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return [field.name for field in self.model._meta.fields if not field.name in ('created_at', 'id')]
+        else:
+            return ('name', 'telegram_id', 'circuit_id',)
